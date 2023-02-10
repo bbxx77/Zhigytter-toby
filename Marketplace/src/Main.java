d8bc7edb07118cc3a5b45a9bb00d346dae83ea9f
daniyaradil04 <124418726+daniyaradil04@users.noreply.github.com>
2023-02-11 01:15:54 +0600

    Main code done

--- Marketplace/src/Main.java
+++ Marketplace/src/Main.java
@@ -1,16 +1,19 @@
-import java.sql.SQLException;
+import java.sql.Connection;
 
 public class Main {
     private static final String DB_USERNAME = "postgres";
     private static final String DB_PASSWORD = "1234567";
     private static final String DB_NAME = "marketplacejava";
 
-    public static void main(String[] args) throws SQLException {
-        User user;
-        do {
-            user = Auth.main(DB_NAME, DB_USERNAME, DB_PASSWORD);
-        } while (user.equals(new User()));
-
-        Run.main(DB_NAME, DB_USERNAME, DB_PASSWORD);
+    public static void main(String[] args)  {
+        DbFunctions db = new DbFunctions();
+        Connection conn = db.connect_to_db(DB_NAME, DB_USERNAME, DB_PASSWORD);
+        while(true) {
+            User user;
+            do {
+                user = Auth.main(conn);
+            } while (user.getId() == -1);
+            Run.main(conn, user);
+        }
     }
 }
