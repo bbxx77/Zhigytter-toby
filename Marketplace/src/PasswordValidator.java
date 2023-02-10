c7521c2cd829c0bac3c24a4be590705d7babd1f5
bbxx77 <124356131+bbxx77@users.noreply.github.com>
2023-02-11 02:02:07 +0600

    Update PasswordValidator.java

--- Marketplace/src/PasswordValidator.java
+++ Marketplace/src/PasswordValidator.java
@@ -2,22 +2,22 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class PasswordValidator {
-    public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_BLUE = "\u001B[34m";
+    private static final String passwordRequirements = 
+        "1. Contains at least one digit (0-9).\n" +
+        "2. Contains at least one lowercase letter (a-z).\n" +
+        "3. Contains at least one uppercase letter (A-Z).\n" +
+        "4. Does not contain whitespace.\n" +
+        "5. Is at least 8 characters long.";
+
     private static final Pattern VALID_PASSWORD_REGEX =
-            Pattern.compile("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\\S+$).{8,}$");
+        Pattern.compile("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\\S+$).{8,}$");
 
     public static void print() {
-        System.out.println(ANSI_BLUE + "1. Contains at least one digit (0-9).\n" +
-                "2. Contains at least one lowercase letter (a-z).\n" +
-                "3. Contains at least one uppercase letter (A-Z).\n" +
-                "4. Does not contain whitespace.\n" +
-                "5. Is at least 8 characters long." +
-                ANSI_RESET);
+        System.out.println(passwordRequirements);
     }
-    public static boolean validate(String password) {
+
+    public static boolean valide(String password) {
         Matcher matcher = VALID_PASSWORD_REGEX.matcher(password);
         return matcher.matches();
     }
-
 }
